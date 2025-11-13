52266e3955a4157368421f0ce170e5b9e4788d50
nogashliz <nogashliz@gmail.com>
2025-11-13 09:02:15 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,5 +1,5 @@
 def approximate_pi(n_terms):
-    n_terms = 0
+    n_series = 0
     for i in range(n_terms):
         n_series += (((-1)**i)/((2*i) + 1))
-    return (4*n_series)
+    return 4 * n_series
